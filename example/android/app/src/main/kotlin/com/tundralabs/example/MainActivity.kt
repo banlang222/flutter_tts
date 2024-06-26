fe3e8e9514c7f64d52b7fc73657361fcf3f6d3fa
banlang222 <master39@126.com>
2024-06-26 23:43:44 +0800

    update .gitignore

---
+++ example/android/app/src/main/kotlin/com/tundralabs/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tundralabs.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ example/lib/main.dart
@@ -0,0 +1,398 @@
+import 'dart:async';
+import 'dart:io' show Platform;
+
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+enum TtsState { playing, stopped, paused, continued }
+
+class _MyAppState extends State<MyApp> {
+  late FlutterTts flutterTts;
+  String? language;
+  String? engine;
+  double volume = 0.5;
+  double pitch = 1.0;
+  double rate = 0.5;
+  bool isCurrentLanguageInstalled = false;
+
+  String? _newVoiceText;
+  int? _inputLength;
+
+  TtsState ttsState = TtsState.stopped;
+
+  bool get isPlaying => ttsState == TtsState.playing;
+  bool get isStopped => ttsState == TtsState.stopped;
+  bool get isPaused => ttsState == TtsState.paused;
+  bool get isContinued => ttsState == TtsState.continued;
+
+  bool get isIOS => !kIsWeb && Platform.isIOS;
+  bool get isAndroid => !kIsWeb && Platform.isAndroid;
+  bool get isWindows => !kIsWeb && Platform.isWindows;
+  bool get isWeb => kIsWeb;
+
+  @override
+  initState() {
+    super.initState();
+    initTts();
+  }
+
+  dynamic initTts() {
+    flutterTts = FlutterTts();
+
+    _setAwaitOptions();
+
+    if (isAndroid) {
+      _getDefaultEngine();
+      _getDefaultVoice();
+    }
+
+    flutterTts.setStartHandler(() {
+      setState(() {
+        print("Playing");
+        ttsState = TtsState.playing;
+      });
+    });
+
+    flutterTts.setCompletionHandler(() {
+      setState(() {
+        print("Complete");
+        ttsState = TtsState.stopped;
+      });
+    });
+
+    flutterTts.setCancelHandler(() {
+      setState(() {
+        print("Cancel");
+        ttsState = TtsState.stopped;
+      });
+    });
+
+    flutterTts.setPauseHandler(() {
+      setState(() {
+        print("Paused");
+        ttsState = TtsState.paused;
+      });
+    });
+
+    flutterTts.setContinueHandler(() {
+      setState(() {
+        print("Continued");
+        ttsState = TtsState.continued;
+      });
+    });
+
+    flutterTts.setErrorHandler((msg) {
+      setState(() {
+        print("error: $msg");
+        ttsState = TtsState.stopped;
+      });
+    });
+  }
+
+  // Future<dynamic> _getLanguages() async => await flutterTts.getLanguages;
+
+  Future<dynamic> _getEngines() async => await flutterTts.getEngines;
+
+  Future<void> _getDefaultEngine() async {
+    var engine = await flutterTts.getDefaultEngine;
+    if (engine != null) {
+      print(engine);
+    }
+  }
+
+  Future<void> _getDefaultVoice() async {
+    var voice = await flutterTts.getDefaultVoice;
+    if (voice != null) {
+      print(voice);
+    }
+  }
+
+  Future<void> _speak() async {
+    print('will speak');
+    await flutterTts.setVolume(volume);
+    await flutterTts.setSpeechRate(rate);
+    await flutterTts.setPitch(pitch);
+
+    _newVoiceText =
+        '''在体育的世界里，永远没有一支球队敢说自己“无敌”，永远没有一名球员敢说自己“永不失败”。正因为此，在 2024 年的欧洲杯 D 组决战中，排名世界第 25 位的奥地利队面对排名世界第七的荷兰队时，依然相信自己可以背水一战，他们也确实险些取得胜利。在这场全球球迷瞩目的比赛中，荷兰队和法国队作为夺冠热门球队，早早就锁定了 16 强的席位。而奥地利队则要面对劲敌荷兰队，他们唯一的出线希望就是在这场比赛中保持不败。虽然看似双方实力有所悬殊，但奥地利队展现出了非凡的斗志和战斗精神，在比赛中多次威胁到荷兰队的球门。
+比赛开始后，奥地利队犹如神兵天降，迅速占据了领先地位。这一切，都要归功于荷兰队那不走运的失误。在比赛进行到第 6 分钟的时候，荷兰前锋马伦在回防时，不小心将奥地利的一记传中球碰进了自家球门，这下子，场上的气氛瞬间变得紧张起来。对于奥地利队而言，这个进球简直是天上掉馅饼，让他们在比赛一开始就取得了领先。
+    ''';
+    if (_newVoiceText != null) {
+      if (_newVoiceText!.isNotEmpty) {
+        print('speak');
+        List<String> sentences =
+            _newVoiceText!.split(RegExp(r'。|，|？|！|,|\?|!'));
+        int progress = 0;
+        for (var i = progress; i < sentences.length; i++) {
+          print('speak text=${sentences[i]}');
+          var r = await flutterTts.speak(sentences[i]);
+          print('r=$r');
+          if (r == 0) {
+            break;
+          } else {
+            progress = i;
+          }
+        }
+      }
+    }
+  }
+
+  Future<void> _setAwaitOptions() async {
+    await flutterTts.awaitSpeakCompletion(true);
+  }
+
+  Future<void> _stop() async {
+    var result = await flutterTts.stop();
+    if (result == 1) setState(() => ttsState = TtsState.stopped);
+  }
+
+  Future<void> _pause() async {
+    var result = await flutterTts.pause();
+    if (result == 1) setState(() => ttsState = TtsState.paused);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    flutterTts.stop();
+  }
+
+  List<DropdownMenuItem<String>> getEnginesDropDownMenuItems(
+      List<dynamic> engines) {
+    var items = <DropdownMenuItem<String>>[];
+    for (dynamic type in engines) {
+      items.add(DropdownMenuItem(
+          value: type as String?, child: Text((type as String))));
+    }
+    return items;
+  }
+
+  void changedEnginesDropDownItem(String? selectedEngine) async {
+    await flutterTts.setEngine(selectedEngine!);
+    language = null;
+    setState(() {
+      engine = selectedEngine;
+    });
+  }
+
+  List<DropdownMenuItem<String>> getLanguageDropDownMenuItems(
+      List<dynamic> languages) {
+    var items = <DropdownMenuItem<String>>[];
+    for (dynamic type in languages) {
+      items.add(DropdownMenuItem(
+          value: type as String?, child: Text((type as String))));
+    }
+    return items;
+  }
+
+  void changedLanguageDropDownItem(String? selectedType) {
+    setState(() {
+      language = selectedType;
+      flutterTts.setLanguage(language!);
+      if (isAndroid) {
+        flutterTts
+            .isLanguageInstalled(language!)
+            .then((value) => isCurrentLanguageInstalled = (value as bool));
+      }
+    });
+  }
+
+  void _onChange(String text) {
+    setState(() {
+      _newVoiceText = text;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter TTS'),
+        ),
+        body: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            children: [
+              _inputSection(),
+              _btnSection(),
+              _engineSection(),
+              // _futureBuilder(),
+              _buildSliders(),
+              if (isAndroid) _getMaxSpeechInputLengthSection(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _engineSection() {
+    if (isAndroid) {
+      return FutureBuilder<dynamic>(
+          future: _getEngines(),
+          builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+            if (snapshot.hasData) {
+              return _enginesDropDownSection(snapshot.data as List<dynamic>);
+            } else if (snapshot.hasError) {
+              return Text('Error loading engines...');
+            } else
+              return Text('Loading engines...');
+          });
+    } else
+      return Container(width: 0, height: 0);
+  }
+
+  // Widget _futureBuilder() => FutureBuilder<dynamic>(
+  //     future: _getLanguages(),
+  //     builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+  //       if (snapshot.hasData) {
+  //         return _languageDropDownSection(snapshot.data as List<dynamic>);
+  //       } else if (snapshot.hasError) {
+  //         return Text('Error loading languages...');
+  //       } else
+  //         return Text('Loading Languages...');
+  //     });
+
+  Widget _inputSection() => Container(
+      alignment: Alignment.topCenter,
+      padding: EdgeInsets.only(top: 25.0, left: 25.0, right: 25.0),
+      child: TextField(
+        maxLines: 11,
+        minLines: 6,
+        onChanged: (String value) {
+          _onChange(value);
+        },
+      ));
+
+  Widget _btnSection() {
+    return Container(
+      padding: EdgeInsets.only(top: 50.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          _buildButtonColumn(Colors.green, Colors.greenAccent, Icons.play_arrow,
+              'PLAY', _speak),
+          _buildButtonColumn(
+              Colors.red, Colors.redAccent, Icons.stop, 'STOP', _stop),
+          _buildButtonColumn(
+              Colors.blue, Colors.blueAccent, Icons.pause, 'PAUSE', _pause),
+        ],
+      ),
+    );
+  }
+
+  Widget _enginesDropDownSection(List<dynamic> engines) => Container(
+        padding: EdgeInsets.only(top: 50.0),
+        child: DropdownButton(
+          value: engine,
+          items: getEnginesDropDownMenuItems(engines),
+          onChanged: changedEnginesDropDownItem,
+        ),
+      );
+
+  Widget _languageDropDownSection(List<dynamic> languages) => Container(
+      padding: EdgeInsets.only(top: 10.0),
+      child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+        DropdownButton(
+          value: language,
+          items: getLanguageDropDownMenuItems(languages),
+          onChanged: changedLanguageDropDownItem,
+        ),
+        Visibility(
+          visible: isAndroid,
+          child: Text("Is installed: $isCurrentLanguageInstalled"),
+        ),
+      ]));
+
+  Column _buildButtonColumn(Color color, Color splashColor, IconData icon,
+      String label, Function func) {
+    return Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          IconButton(
+              icon: Icon(icon),
+              color: color,
+              splashColor: splashColor,
+              onPressed: () => func()),
+          Container(
+              margin: const EdgeInsets.only(top: 8.0),
+              child: Text(label,
+                  style: TextStyle(
+                      fontSize: 12.0,
+                      fontWeight: FontWeight.w400,
+                      color: color)))
+        ]);
+  }
+
+  Widget _getMaxSpeechInputLengthSection() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        ElevatedButton(
+          child: Text('Get max speech input length'),
+          onPressed: () async {
+            _inputLength = await flutterTts.getMaxSpeechInputLength;
+            setState(() {});
+          },
+        ),
+        Text("$_inputLength characters"),
+      ],
+    );
+  }
+
+  Widget _buildSliders() {
+    return Column(
+      children: [_volume(), _pitch(), _rate()],
+    );
+  }
+
+  Widget _volume() {
+    return Slider(
+        value: volume,
+        onChanged: (newVolume) {
+          setState(() => volume = newVolume);
+        },
+        min: 0.0,
+        max: 1.0,
+        divisions: 10,
+        label: "Volume: $volume");
+  }
+
+  Widget _pitch() {
+    return Slider(
+      value: pitch,
+      onChanged: (newPitch) {
+        setState(() => pitch = newPitch);
+      },
+      min: 0.5,
+      max: 2.0,
+      divisions: 15,
+      label: "Pitch: $pitch",
+      activeColor: Colors.red,
+    );
+  }
+
+  Widget _rate() {
+    return Slider(
+      value: rate,
+      onChanged: (newRate) {
+        setState(() => rate = newRate);
+      },
+      min: 0.0,
+      max: 1.0,
+      divisions: 10,
+      label: "Rate: $rate",
+      activeColor: Colors.green,
+    );
+  }
+}
